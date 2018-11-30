1923baeaedbc3543aac9314b0426ed948afa51c2
Gautam Mittal <gautam@mittal.net>
2018-11-30 14:55:13 -0800

    Add .gitignore

--- data/reddit-selfposts/analyze.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-comments = pd.read_csv('reddit-analyze.csv')
-sr_info = pd.read_csv('subreddit-info.csv')
-
-print(comments.groupby('subreddit').mean().nlargest(10, 'toxic'))
-    
